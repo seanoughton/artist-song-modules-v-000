cb557b1fdbfdbcd9256f1d6f09601857f9231a04
Sean Oughton <seanoughton@gmail.com>
2018-02-12 23:19:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,7 @@ class Artist
 
   @@artists = []
 
-  #def self.find_by_name(name)
-    #@@artists.detect{|a| a.name == name}
-  #end
-
   def initialize
-    #@@artists << self
-    #self.class.all << self
     super
     @songs = []
   end
@@ -24,14 +18,6 @@ class Artist
   def self.all
     @@artists
   end
-
-  #def self.reset_all
-    #self.all.clear
-  #end
-
-  #def self.count
-    #@@artists.count
-  #end
 
   def add_song(song)
     @songs << song
@@ -41,12 +27,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  #def to_param
-    #name.downcase.gsub(' ', '-')
-  #end
-
-
-
 
 end
